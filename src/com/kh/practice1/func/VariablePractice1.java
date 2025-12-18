675dc7c8781c75154978622f23e4efc80fc7ca96
user1 <user1@DESKTOP-H488MRD>
2025-12-18 16:53:18 +0900

    2일차 수업내영

---
+++ src/com/kh/practice1/func/VariablePractice1.java
@@ -0,0 +1,25 @@
+package com.kh.practice1.func;
+
+import java.util.Scanner;
+
+public class VariablePractice1 {
+	
+	public void practice1() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이름 : ");
+		String name = sc.next();
+		
+		System.out.print("성별 : ");
+		char gender = sc.next().charAt(0);
+		
+		System.out.print("나이 : ");
+		int age = sc.nextInt();
+		
+		System.out.print("키 : ");
+		double height = sc.nextDouble();
+		
+	}
+	
+	
+}
